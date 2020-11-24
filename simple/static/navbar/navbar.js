b4efbc17df2f8e67a093e1c4c7909a93e7a2762d
jchen42703 <jchen42703@gmail.com>
2020-11-24 18:07:35 -0500

    Simple NavBar and Hero Image changes

--- simple/static/navbar/navbar.js
+++ simple/static/navbar/navbar.js
@@ -1,39 +1,5 @@
-const navbar = document.getElementById("navbar");
-const navbarToggle = navbar.querySelector(".navbar-toggle");
-
-function openMobileNavbar() {
-  navbar.classList.add("opened");
-  navbarToggle.setAttribute("aria-label", "Close navigation menu.");
-}
-
-function closeMobileNavbar() {
-  navbar.classList.remove("opened");
-  navbarToggle.setAttribute("aria-label", "Open navigation menu.");
-}
-
-navbarToggle.addEventListener("click", () => {
-  if (navbar.classList.contains("opened")) {
-    closeMobileNavbar();
-  } else {
-    openMobileNavbar();
-  }
+var menuToggle = document.querySelector(".menu-toggle");
+menuToggle.addEventListener("click", (e) => {
+  document.querySelector(".nav").classList.toggle("mobile-nav");
+  menuToggle.classList.toggle("is-active");
 });
-
-const navbarMenu = navbar.querySelector(".navbar-menu");
-const navbarLinksContainer = navbar.querySelector(".navbar-links");
-
-navbarLinksContainer.addEventListener("click", (clickEvent) => {
-  clickEvent.stopPropagation();
-});
-
-navbarMenu.addEventListener("click", closeMobileNavbar);
-
-document
-  .getElementById("options")
-  .querySelectorAll("input[name='navtype']")
-  .forEach((option) => {
-    option.addEventListener("change", (e) => {
-      const navType = e.target.id.split("-").join(" ");
-      navbarMenu.classList = "navbar-menu " + navType;
-    });
-  });
