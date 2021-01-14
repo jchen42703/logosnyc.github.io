321ea0e6fbd8c9aa465c1cfcd4d54d3ffb946ed0
jchen42703 <jchen42703@gmail.com>
2021-01-13 21:28:33 -0500

    Added support for dropdown menu in navbar for the CONNECT

--- static/navbar.js
+++ static/navbar.js
@@ -1,5 +1,28 @@
 var menuToggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".nav-links-container");
+const items = document.querySelectorAll(".nav-item");
+
 menuToggle.addEventListener("click", (e) => {
-  document.querySelector(".nav").classList.toggle("mobile-nav");
+  document.querySelector(".nav-links-container").classList.toggle("mobile-nav");
   menuToggle.classList.toggle("is-active");
 });
+
+function toggleDrop() {
+  if (this.classList.contains("drop-active")) {
+    this.classList.remove("drop-active");
+  } else if (menu.querySelector(".drop-active")) {
+    menu.querySelector(".drop-active").classList.remove("drop-active");
+    this.classList.add("drop-active");
+  } else {
+    this.classList.add("drop-active");
+  }
+}
+
+// tablet -> 768px (in _general.scss)
+if (document.documentElement.clientWidth < 768) {
+  for (let item of items) {
+    if (item.querySelector(".drop")) {
+      item.addEventListener("click", toggleDrop, false);
+    }
+  }
+}
